627d1ac646efbeacb77ba97b60795f363cf7ecaf
Jack Parker <jprkr96@gmail.com>
2025-07-11 23:22:19 -0500

    init-2

---
+++ vsapilot/src/defaultPrompt.ts
@@ -0,0 +1,1 @@
+let defaultPrompt: string = 'Using only the UI Components found at this url: "https://design.visa.com/components/", create any new content strictly with the library of UI components at that url. When asked to give references to UI components make a list of components used for the final product.';
